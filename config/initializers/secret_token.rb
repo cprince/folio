9b0be20dd5454bc72a83250d8b04296551960d06
Colin Prince <colin.prince@utoronto.ca>
2011-07-29 16:00:21 -0400

    initial up to date

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Folio::Application.config.secret_token = 'bd79aedbd4f81503775d396475d046819841df8d4008dec789d913425ffd7311e415a3675da237b120409000ee9fa9d4f158a0bbfc8920509ee6cf86dbf4aa5d'
