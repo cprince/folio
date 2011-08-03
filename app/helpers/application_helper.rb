867b3acf30d84be09367acc67ce16f11f1722b1a
Colin Prince <col@colinprince.com>
2011-08-03 11:41:36 -0400

    start view mods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # return a suitable title for static pages
+  def title
+    base_title = "Ruby Folio App"
+    if @title.nil?
+      base_title
+    else
+      base_title << " | " << @title
+      # "#{base_title} | #{@title}"
+    end
+  end
+
 end
