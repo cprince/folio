6c0af95fda0fb5b4bf58ec672c23d815b235a4c8
Colin Prince <colin.prince@utoronto.ca>
2011-08-08 12:03:01 -0400

    User signup and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,9 +46,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to the Folio App!"
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
+        @title = "Sign Up"
         format.html { render :action => "new" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
